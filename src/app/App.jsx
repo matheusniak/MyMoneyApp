6fd1f7412d7bde231fd4f13b1e16641979998296
matheusniak <233319463+matheusniak@users.noreply.github.com>
2025-10-01 06:44:43 -0300

    style(app): importar e integrar Header/Sidebar/Footer/Messages components

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import Header from "../common/template/Header";
+import Sidebar from "../common/template/SideBar";
+import Footer from "../common/template/Footer";
+import Messages from "../common/template/Messages";
+
 export default function App() {
   return (
     <div className="wrapper">
