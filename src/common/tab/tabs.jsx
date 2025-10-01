406244ed1b0e0435bb53bf279dc1783a32fafda9
matheusniak <233319463+matheusniak@users.noreply.github.com>
2025-10-01 04:00:27 -0300

    feat(tabs): add basic Tabs, TabsHeader and TabsContent

---
+++ src/common/tab/tabs.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default ({ children }) => (
+  <div className="nav-tabs-custom">{children}</div>
+);
---
+++ src/common/tab/tabsHeader.jsx
@@ -0,0 +1,2 @@
+import React from "react";
+export default ({ children }) => <ul className="nav nav-tabs">{children}</ul>;
