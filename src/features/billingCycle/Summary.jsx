2fdb9ce3e8f2271eff5be3ef2b35049885317118
matheusniak <233319463+matheusniak@users.noreply.github.com>
2025-10-01 05:26:27 -0300

    feat(billing): adicionar componente Summary (credit/debt/value)

---
+++ src/features/billingCycle/Summary.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Grid from "../../common/layout/Grid";
+import Row from "../../common/layout/Row";
+import ValueBox from "../../common/widget/ValueBox";
+
+export default function Summary({ credit = 0, debt = 0 }) {
+  return (
+    <Grid cols="12">
+      <fieldset>
+        <legend>Resumo</legend>
+        <Row>
+          <ValueBox
+            cols="12 4"
+            color="green"
+            icon="bank"
+            value={`R$ ${credit}`}
+            text="Total de Créditos"
+          />
+          <ValueBox
+            cols="12 4"
+            color="red"
+            icon="credit-card"
+            value={`R$ ${debt}`}
+            text="Total de Débitos"
+          />
+          <ValueBox
+            cols="12 4"
+            color="blue"
+            icon="money"
+            value={`R$ ${credit - debt}`}
+            text="Valor Consolidado"
+          />
+        </Row>
+      </fieldset>
+    </Grid>
+  );
+}
