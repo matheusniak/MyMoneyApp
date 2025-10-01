3de0839e0ec93c3b688c8075ffd4b1f07af7f260
matheusniak <233319463+matheusniak@users.noreply.github.com>
2025-10-01 05:34:28 -0300

    feat(billing-slice): adicionar thunks create/update/remove e sucesso (toast)

--- src/features/billingCycle/billingCycleSlice.js
+++ src/features/billingCycle/billingCycleSlice.js
@@ -2,10 +2,44 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../../mocks/axiosMock";
 import { toast } from "react-toastify";
 
+const BASE_URL = "http://localhost:3003/api";
+
 export const fetchBillingCycles = createAsyncThunk(
   "billing/fetchList",
   async () => {
     const resp = await axios.get(`${BASE_URL}/billingCycles`);
+    return resp.data;
+  }
+);
+
+export const createBillingCycle = createAsyncThunk(
+  "billing/create",
+  async (values, { dispatch }) => {
+    const resp = await axios.post(`${BASE_URL}/billingCycles`, values);
+    toast.success("Operação realizada com sucesso.");
+    dispatch(fetchBillingCycles());
+    return resp.data;
+  }
+);
+
+export const updateBillingCycle = createAsyncThunk(
+  "billing/update",
+  async (values, { dispatch }) => {
+    const id = values._id || "";
+    const resp = await axios.put(`${BASE_URL}/billingCycles/${id}`, values);
+    toast.success("Operação realizada com sucesso.");
+    dispatch(fetchBillingCycles());
+    return resp.data;
+  }
+);
+
+export const removeBillingCycle = createAsyncThunk(
+  "billing/remove",
+  async (values, { dispatch }) => {
+    const id = values._id || "";
+    const resp = await axios.delete(`${BASE_URL}/billingCycles/${id}`);
+    toast.success("Operação realizada com sucesso.");
+    dispatch(fetchBillingCycles());
     return resp.data;
   }
 );
@@ -23,7 +57,6 @@ const billingSlice = createSlice({
     formInitialValues: initialValues,
     status: "idle",
   },
-
   reducers: {
     setFormValues(state, action) {
       state.formInitialValues = action.payload;
